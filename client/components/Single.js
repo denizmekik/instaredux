1e6a29b3723a82774a40c46580b46b8abcca68c6
Deniz Mekik <denizmekik@gmail.com>
2016-09-21 19:43:06 -0700

    set up router

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+      I am single photo
+      </div>
+    )
+  }
+})
+
+export default Single;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import { render } from 'react-dom';
+import { Router, Route, browerserHistory, IndexRoute} from 'react-router'
 
 //import css
 import css from './styles/style.styl';
 
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
-render(<Main/>, document.getElementById('root'));
+const routes = (
+  <Router history={browerserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(route, document.getElementById('root'));
 
 
 
